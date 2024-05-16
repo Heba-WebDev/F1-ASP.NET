0824776a6d9ea1708b3021f46139da577e61e859
Heba-Webdev23 <jtvonline@yahoo.com>
2024-05-17 00:48:39 +0300

    Team's class

---
+++ Models/Team.cs
@@ -0,0 +1,10 @@
+namespace formulaone.Models;
+
+public class Team
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Country { get; set; }
+
+    public string? TeamPrincipal { get; set; }
+}
